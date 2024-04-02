cba9a8d3469a730cef7f3cf00f7de77fc74b9c05
XakerCool <xakercool33@gmail.com>
2024-04-02 16:50:42 +0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'schoolpro',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
